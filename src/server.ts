fd25dd7eb229a55ffbc04ddf289365047053e666
rafaelAmora <rmoreira2209@gmail.com>
2026-07-08 13:46:57 -0300

    add_cors-ajuste

--- src/server.ts
+++ src/server.ts
@@ -8,17 +8,17 @@ import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.use(express.json());
 app.use(router);
 app.use(errorHadling);
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
 
 async function bootstrap() {
   await seedAdmin();
