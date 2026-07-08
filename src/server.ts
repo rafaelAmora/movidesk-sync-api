48ec4a2e7788ee878f22cbfcb8d905ed7185d030
rafaelAmora <rmoreira2209@gmail.com>
2026-07-08 13:43:03 -0300

    add_cors

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,21 @@ import router from "./routes/index.js";
 import "dotenv/config";
 import { errorHadling } from "./middlewares/errorHandling.js";
 import { seedAdmin } from "./utils/seedAdmin.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
 app.use(errorHadling);
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 async function bootstrap() {
   await seedAdmin();
