3c4f3098ea6eb7394eaf317a42b6adc3a621a7c5
rafaelAmora <rmoreira2209@gmail.com>
2026-07-08 13:50:46 -0300

    add_cors-ajuste credenciais

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,7 @@ app.use(
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   }),
 );
 
